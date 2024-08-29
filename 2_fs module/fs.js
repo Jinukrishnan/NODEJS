0a704c55825d768331abfdc5651b0d72078e166a
jinukrishnan <jinukrishnan.p@gmail.com>
2024-08-29 11:04:14 +0530

    ads

--- 2_fs module/fs.js
+++ 2_fs module/fs.js
@@ -1,9 +1,65 @@
 const fs= require("fs")
 
-fs.unlink("./message.txt",(error)=>{
-    if(error)
-        {
-            console.log("unable to delete");
-        }
+// write file
+// =============================================================
+// fs.writeFileSync("message.txt","hai team",(error)=>{
+//     if(error){
+//         console.log("unableto write");
         
-})
+//     }
+// })
+// =============================================================
+// append file
+// =============================================================
+// fs.appendFileSync("message.txt","data added",(error)=>{
+//         if(error){
+//         console.log("unableto update");
+        
+//     }
+// })
+// =============================================================
+// read file
+// =============================================================
+// fs.readFile("message.txt","utf-8",(error,data)=>{
+//             if(error){
+//         console.log("unableto update");
+        
+//     }
+//     else{
+//         console.log(data);
+        
+//     }
+// })
+// =============================================================
+// unlink file 
+// =============================================================
+// fs.unlink("./message.txt",(error)=>{
+//     if(error)
+//         {
+//             console.log("unable to delete");
+//         }
+        
+// })
+// =============================================================
+// create folder
+// =============================================================
+// fs.mkdir("hello",(error)=>{
+//     if (error) {
+//         console.log(error);
+        
+//     }
+// })
+// =============================================================
+// read files in side folder
+// =============================================================
+// fs.readdir("hello",(error,data)=>{
+//     if (error) {
+//         console.log(error);
+        
+//     }
+//     else{
+//         console.log(data);
+        
+//     }
+// })
+// =============================================================
